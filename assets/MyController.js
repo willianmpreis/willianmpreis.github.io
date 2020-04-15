7e4c51e9d209efc774f2f233f871794ceb466a15
Willian Reis <willian.fagoc@gmail.com>
2020-04-14 23:28:37 -0300

    Add home page

---
+++ assets/MyController.js
@@ -0,0 +1,69 @@
+class MyController
+{
+    baseUrl = "https://api.github.com/users/";
+    baseUser = "willianmpreis";
+    profilePicture = '';
+    birth = '1990-05-04';
+    
+
+    constructor()
+    {
+        this.getElements();
+        this.render();
+    }
+
+    getElements()
+    {
+        this.avatar = document.querySelector("#avatar");
+        this.title = document.querySelector("#title");
+        this.age = document.querySelector('#age');
+    }
+
+    render()
+    {
+        this.ajax(`${this.baseUrl}${this.baseUser}`).then(response => {
+            this.profilePicture = response.avatar_url;
+            if (this.avatar) {
+                this.avatar.style = `background-image: url(${this.profilePicture};)`;
+                console.log(this.avatar);
+            }
+            if (this.title) {
+                this.title.innerHTML = response.bio;
+            }
+            if (this.age) {
+                let age = this.calcAge(this.birth);
+                this.age.innerHTML = `${age} anos`;
+            }    
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    calcAge(dateString) {
+       var birthday = +new Date(dateString);
+       return ~~((Date.now() - birthday) / (31557600000));
+    }
+
+    ajax(url, method = 'GET', onprogress = () => {})
+    {
+        return new Promise((resolve, reject) => {
+            let ajax = new XMLHttpRequest();                
+                ajax.open(method, url);
+
+                ajax.onload = event => {
+                    try {
+                        resolve(JSON.parse(ajax.responseText))
+                    } catch (e) {
+                        reject(e)                        ;
+                    }
+                }
+
+                ajax.onerror = event => {
+                    reject(event);
+                }
+
+                ajax.send();
+        });
+    }
+}
---
+++ assets/index.js
@@ -0,0 +1,1 @@
+window.myController = new MyController();
